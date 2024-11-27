3649c18182f6ca3c49bc1e7089b1abfd7925179f
RicardoVCampos <rvc.top@gmail.com>
2024-11-26 21:55:54 -0300

    comandos para o browser

---
+++ app.js
@@ -0,0 +1,167 @@
+// let var1 = "cachorro"
+// var1 = "chiuaua"
+// console.log(var1)
+
+// const constante1 = "chuchu"
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let array4 = [
+//     [1, 2, 3],    
+//     [false, null, "Texto", ["Oi som", 2, true]], 
+//     [42, "JavaScript"],     
+//     [true, 7.5, "Final"]   
+// ]    
+// // Preciso que seja renderizado no console null do array 
+// //Preciso que seja renderizado no console Oi som
+// //Preciso que seja renderizado Final no console
+
+// console.log(array4[1][1])
+
+// console.log(array4[1][3][0])
+
+// console.log(array4[3][2])
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let carro = {
+//     id:1,
+//     modelo:"Fusca",
+//     ano:1979,
+//     cor:"Amarela"
+// }
+
+// console.log(carro.modelo,carro.ano)
+
+// console.log(`O carro ${carro.modelo} do ano ${carro.ano} já não paga mais IPVA.`)
+
+// console.log(`Temos ${carro.id} carro modelo ${carro.modelo} disponível, ele é do ano ${carro.ano} e da cor ${carro.cor}.`)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// let carros = [
+//     { 
+//         id: 1,
+//         modelo: "Fusca",
+//         ano: 1979,
+//         cor: "Amarela"
+//     },
+//     { 
+//         id: 2,
+//         modelo: "Gol",
+//         ano: 1990,
+//         cor: "Azul"
+//     },
+//     { 
+//         id: 3,
+//         modelo: "Civic",
+//         ano: 2005,
+//         cor: "Preta"
+//     },
+//     { 
+//         id: 4,
+//         modelo: "Palio",
+//         ano: 2002,
+//         cor: "Vermelha"
+//     },
+//     { 
+//         id: 5,
+//         modelo: "Fiesta",
+//         ano: 2010,
+//         cor: "Branca"
+//     },
+//     { 
+//         id: 6,
+//         modelo: "Kangoo",
+//         ano: 2015,
+//         cor: "Prata"
+//     },
+//     { 
+//         id: 7,
+//         modelo: "Uno",
+//         ano: 1995,
+//         cor: "Verde"
+//     },
+//     { 
+//         id: 8,
+//         modelo: "Saveiro",
+//         ano: 2012,
+//         cor: "Preta"
+//     },
+//     { 
+//         id: 9,
+//         modelo: "Strada",
+//         ano: 2017,
+//         cor: "Branca"
+//     },
+//     { 
+//         id: 10,
+//         modelo: "Siena",
+//         ano: 2008,
+//         cor: "Cinza"
+//     }
+// ];
+
+// ////////////////////////////////////////////////////////////////////////////////////
+
+// // let carro = `O carro que eu tenho intenção de comprar é o ${carros[1].modelo}.`
+
+// // console.log(carro)
+
+// ////////////////////////////////////////////////////////////////////////////////////
+
+// Exercício de fixação da tarefa acima.
+
+// // Acesse o modelo do carro que tem o id 3 e exiba uma frase com seu nome.
+// console.log(`O ${carros[2].modelo} é um carro tão bom como os Corolla da Toyota.`)
+
+// // Acesse a cor do carro com o modelo "Fiesta" e crie uma frase informando a cor do carro.
+// console.log(`Tem um carro modelo ${carros[4].modelo} de cor ${carros[4].cor} disponível para troca.`)
+
+// // Crie uma frase dizendo o ano do carro que tem o id 7
+// console.log(`O carro ${carros[6].modelo} é de ano ${carros[6].ano}.`)
+
+// // Exiba o modelo e a cor do carro que foi fabricado em 2005.
+// console.log(`O Honda ${carros[2].modelo} tem na cor ${carros[2].cor} e é do ano ${carros[2].ano}.`)
+
+// // Crie uma frase que diga o modelo e o ano do carro com id 10.
+// console.log(`Achei o ${carros[9].modelo} ${carros[9].ano} do jeito que eu queria.`)
+
+// // Crie uma frase dizendo que o carro com o id 5 é branco e foi fabricado em 2010.
+// console.log(`O Ford ${carros[4].modelo} é de cor ${carros[4].cor} e fabricado do ano ${carros[4].ano}.`)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// alert("A turma FS20 é a melhor turma da Digital College.")
+
+// prompt("Digite seu nome")
+
+// aqui aprendemos a criar pop-up no browser
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let nome = prompt("Digite seu nome")
+// alert(`Bem vindo ${nome}`)
+
+// aqui vai aparecer no browser o prompit para digitar e depois a mensagem somada ao prompit
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let numero1 = Number(prompt("Digite número 1"))
+// let numero2 = Number(prompt("Digite número 2"))
+// let soma = numero1 + numero2
+
+// alert(`O resultado a soma é: ${soma}.`)
+
+// aqui vai aparecer a soma das duas variaveis
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+let numero1 = Number(prompt("Digite número 1"))
+let numero2 = Number(prompt("Digite número 2"))
+let soma = numero1 + numero2
+
+alert(`O resultado a soma é: ${soma}.`)
+
