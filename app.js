91cb2a2113cbf36a4779b7d60354dfe37b2f0419
RicardoVCampos <rvc.top@gmail.com>
2024-12-03 18:38:46 -0300

    exercicios do browser

--- app.js
+++ app.js
@@ -167,89 +167,140 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let filmes = [
-    { 
-        id: 1,
-        titulo: "O Senhor dos Anéis: A Sociedade do Anel",
-        ano: 2001,
-        genero: "Fantasia"
-    },
-    { 
-        id: 2,
-        titulo: "O Poderoso Chefão",
-        ano: 1972,
-        genero: "Crime"
-    },
-    { 
-        id: 3,
-        titulo: "Forrest Gump: O Contador de Histórias",
-        ano: 1994,
-        genero: "Drama"
-    },
-    { 
-        id: 4,
-        titulo: "Star Wars: O Império Contra-Ataca",
-        ano: 1980,
-        genero: "Ficção científica"
-    },
-    { 
-        id: 5,
-        titulo: "A Origem",
-        ano: 2010,
-        genero: "Ficção científica"
-    },
-    { 
-        id: 6,
-        titulo: "O Rei Leão",
-        ano: 1994,
-        genero: "Animação"
-    },
-    { 
-        id: 7,
-        titulo: "Titanic",
-        ano: 1997,
-        genero: "Romance"
-    },
-    { 
-        id: 8,
-        titulo: "Vingadores: Ultimato",
-        ano: 2019,
-        genero: "Ação"
-    },
-    { 
-        id: 9,
-        titulo: "O Exorcista",
-        ano: 1973,
-        genero: "Terror"
-    },
-    { 
-        id: 10,
-        titulo: "Jurassic Park",
-        ano: 1993,
-        genero: "Aventura"
-    }	
-]; 
+// let filmes = [
+//     { 
+//         id: 1,
+//         titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+//         ano: 2001,
+//         genero: "Fantasia"
+//     },
+//     { 
+//         id: 2,
+//         titulo: "O Poderoso Chefão",
+//         ano: 1972,
+//         genero: "Crime"
+//     },
+//     { 
+//         id: 3,
+//         titulo: "Forrest Gump: O Contador de Histórias",
+//         ano: 1994,
+//         genero: "Drama"
+//     },
+//     { 
+//         id: 4,
+//         titulo: "Star Wars: O Império Contra-Ataca",
+//         ano: 1980,
+//         genero: "Ficção científica"
+//     },
+//     { 
+//         id: 5,
+//         titulo: "A Origem",
+//         ano: 2010,
+//         genero: "Ficção científica"
+//     },
+//     { 
+//         id: 6,
+//         titulo: "O Rei Leão",
+//         ano: 1994,
+//         genero: "Animação"
+//     },
+//     { 
+//         id: 7,
+//         titulo: "Titanic",
+//         ano: 1997,
+//         genero: "Romance"
+//     },
+//     { 
+//         id: 8,
+//         titulo: "Vingadores: Ultimato",
+//         ano: 2019,
+//         genero: "Ação"
+//     },
+//     { 
+//         id: 9,
+//         titulo: "O Exorcista",
+//         ano: 1973,
+//         genero: "Terror"
+//     },
+//     { 
+//         id: 10,
+//         titulo: "Jurassic Park",
+//         ano: 1993,
+//         genero: "Aventura"
+//     }	
+// ]; 
 
-// Acesse o título do filme com o id 4 e exiba uma frase dizendo que o filme é do gênero "Ficção científica".
-console.log(`O filme ${filmes[3].titulo} é do gênero ${filmes[3].genero}.`)
+// // Acesse o título do filme com o id 4 e exiba uma frase dizendo que o filme é do gênero "Ficção científica".
+// console.log(`O filme ${filmes[3].titulo} é do gênero ${filmes[3].genero}.`)
 
-// Acesse o gênero do filme "Titanic" e crie uma frase dizendo o gênero do filme.
-console.log(`O filme ${filmes[6].titulo} é do tipo de ${filmes[6].genero} que faz chorar.`)
+// // Acesse o gênero do filme "Titanic" e crie uma frase dizendo o gênero do filme.
+// console.log(`O filme ${filmes[6].titulo} é do tipo de ${filmes[6].genero} que faz chorar.`)
 
-// Crie uma frase dizendo o ano do filme que tem o id 7.
-console.log(`O filme ${filmes[7].titulo} foi o melhor filme feito no ano ${filmes[6].ano}.`)
+// // Crie uma frase dizendo o ano do filme que tem o id 7.
+// console.log(`O filme ${filmes[7].titulo} foi o melhor filme feito no ano ${filmes[6].ano}.`)
 
-// Exiba o título e o ano do filme que foi lançado em 1994.
-console.log(`O filme ${filmes[5].titulo} foi lançado no ano de ${filmes[5].ano}.`)
+// // Exiba o título e o ano do filme que foi lançado em 1994.
+// console.log(`O filme ${filmes[5].titulo} foi lançado no ano de ${filmes[5].ano}.`)
 
-// Crie uma frase dizendo o título e o ano do filme com id 3.
-console.log(`No ano de ${filmes[2].ano} foi lançado o filme ${filmes[2].titulo}.`)
+// // Crie uma frase dizendo o título e o ano do filme com id 3.
+// console.log(`No ano de ${filmes[2].ano} foi lançado o filme ${filmes[2].titulo}.`)
 
-// Crie uma frase dizendo que o filme com o id 10 é de "Aventura" e foi lançado em 1993.
-console.log(`No ano de ${filmes[9].ano} foi lançado um filme de ${filmes[9].genero} chamado ${filmes[9].titulo}.`)
+// // Crie uma frase dizendo que o filme com o id 10 é de "Aventura" e foi lançado em 1993.
+// console.log(`No ano de ${filmes[9].ano} foi lançado um filme de ${filmes[9].genero} chamado ${filmes[9].titulo}.`)
 
-// Acesse o título do filme que é de gênero "Terror" e crie uma frase dizendo que ele é do gênero "Terror".
-console.log(`O filme ${filmes[8].titulo} é um filme de ${filmes[8].genero} que poucos conseguem assistir até o final.`)
+// // Acesse o título do filme que é de gênero "Terror" e crie uma frase dizendo que ele é do gênero "Terror".
+// console.log(`O filme ${filmes[8].titulo} é um filme de ${filmes[8].genero} que poucos conseguem assistir até o final.`)
 
-// Exiba o título e o gênero do filme que foi lançado em 2001.
-console.log(`O melhor filme de ${filmes[0].genero} lançado em ${filmes[0].ano} com maior bilheteria foi o ${filmes[0].titulo}.`)
+// // Exiba o título e o gênero do filme que foi lançado em 2001.
+// console.log(`O melhor filme de ${filmes[0].genero} lançado em ${filmes[0].ano} com maior bilheteria foi o ${filmes[0].titulo}.`)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Peça ao usuário para digitar dois números. Depois, calcule e exiba o resultado da multiplicação desses dois números em uma mensagem. 
+
+// let numero1 = Number(prompt("Digite número 1"))
+// let numero2 = Number(prompt("Digite número 2"))
+// let multiplicacao = numero1 * numero2
+
+// alert(`O resultado da multiplicação é: ${multiplicacao}.`)
+
+
+// Solicite ao usuário que digite dois números. Em seguida, calcule e exiba o resultado da divisão do primeiro número pelo segundo. 
+
+// let numero1 = Number(prompt("Digite número 1"))
+// let numero2 = Number(prompt("Digite número 2"))
+// let divisao = numero1 / numero2
+
+// alert(`O resultado da divisão é: ${divisao}.`)
+
+// Peça ao usuário para inserir dois números. Calcule a subtração do primeiro número pelo segundo e exiba o resultado em uma mensagem.
+
+// let numero1 = Number(prompt("Digite número 1"))
+// let numero2 = Number(prompt("Digite número 2"))
+// let subtracao = numero1 - numero2
+
+// alert(`O resultado da subtração é: ${subtracao}.`)
+
+
+//  Solicite ao usuário dois números e calcule a multiplicação entre eles. Exiba a mensagem "X vezes Y é igual a Z", substituindo X, Y e Z pelos números e o resultado.
+
+// let numero1 = Number(prompt("Digite número 1"))
+// let numero2 = Number(prompt("Digite número 2"))
+// let multiplicacao = numero1 * numero2
+
+// alert(`O resultado da multiplicação do número ${numero1}, pelo número ${numero2}, é igual a ${multiplicacao}`)
+
+// Solicite ao usuário dois números negativos. Calcule a multiplicação entre eles e exiba o resultado.
+
+// let numero1 = Number(prompt("Digite o número negativo 1"))
+// let numero2 = Number(prompt("Digite o número negativo 2"))
+// let multiplicacao = numero1 * numero2
+
+// alert(`O resultado da multiplicação é: ${multiplicacao}.`)
+
+
+// Peca para o usuario digitar seu nome e aparecer no alert o nome do usuario com uma mensagem de bem vindo
+
+// let nome = prompt("Digite seu nome")
+// alert(`Bem vindo ${nome}`)
